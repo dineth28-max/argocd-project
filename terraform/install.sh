9ad8331b70a2ffcba3b6337a02875befa6abc32e
dineth28-max <sandakelumdineth120@gmail.com>
2026-02-17 23:30:56 +0530

    first commit

---
+++ terraform/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update -y
+apt-get upgrade -y  
+
+apt-get install -y curl
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
