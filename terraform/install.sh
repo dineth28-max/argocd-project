f7102b3e759827dd11a299cb1da6ed67e448d15c
dineth28-max <sandakelumdineth120@gmail.com>
2026-02-18 00:17:01 +0530

    2nd

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-apt-get update -y
-apt-get upgrade -y  
 
-apt-get install -y curl
+# Update and upgrade system packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
+# Install curl if not already installed
+sudo apt-get install -y curl
+
+# Download the latest stable version of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
+# Make kubectl executable and move it to /usr/local/bin
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
+
+# Verify kubectl installation
+kubectl version --client
+
+# Enable CoreDNS in MicroK8s
+sudo microk8s enable dns
+
+# Restart MicroK8s to apply changes
+sudo microk8s stop
+sudo microk8s start
+
+# install ArgoCD in k8s
+kubectl create namespace argocd
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# access ArgoCD UI
+kubectl get svc -n argocd
+kubectl port-forward svc/argocd-server 8080:443 -n argocd
